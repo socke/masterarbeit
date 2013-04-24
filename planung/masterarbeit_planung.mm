983d772aa78298903eb26ce4117926f622e123ee
Stefan Sobek <stefan.sobek@t-systems.com>
2013-04-24 16:49:04 +0200

    Projektplan

---
+++ planung/masterarbeit_planung.mm
@@ -0,0 +1,48 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<attribute_registry>
+<attribute_name NAME="Phone" VISIBLE="true"/>
+<attribute_name NAME="email" VISIBLE="true"/>
+</attribute_registry>
+<node CREATED="1366546924889" ID="ID_1414241836" MODIFIED="1366550076946" TEXT="Masterarbeit Planung">
+<node CREATED="1366546940449" ID="ID_1585242961" MODIFIED="1366548716808" POSITION="right" TEXT="Anforderungen">
+<node CREATED="1366546953103" ID="ID_1758159908" MODIFIED="1366546993826" TEXT="Use Cases">
+<node CREATED="1366546956994" ID="ID_47315909" MODIFIED="1366547228062" TEXT="Haupt-usecases erstellen">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+</node>
+<node CREATED="1366547003927" ID="ID_1669341954" MODIFIED="1366550270159" POSITION="right" TEXT="Aufgabenbeschreibung">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1366547147353" ID="ID_1975087999" MODIFIED="1366547268481" TEXT="Zielsetzung">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1366547236239" ID="ID_644151253" MODIFIED="1366547270359" TEXT="Vorgaben">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1366547256027" ID="ID_1435652613" MODIFIED="1366547270786" TEXT="Abgrenzung">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1366547260989" ID="ID_1035267474" MODIFIED="1366547271068" TEXT="Detailbeschreibung">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node CREATED="1366547340278" ID="ID_137402585" MODIFIED="1366550275278" POSITION="right" TEXT="Machbarkeit und erster Prototyp">
+<node CREATED="1366547376818" ID="ID_650029879" MODIFIED="1366547381915" TEXT="Auswahl Web Server">
+<node CREATED="1366547512019" ID="ID_67646618" MODIFIED="1366547515688" TEXT="Begr&#xfc;ndung der Auswahl"/>
+</node>
+<node CREATED="1366547357333" ID="ID_1446097906" MODIFIED="1366547370035" TEXT="Auswahl Web Service"/>
+<node CREATED="1366547370270" ID="ID_808154954" MODIFIED="1366547373038" TEXT="Test Web Service"/>
+</node>
+<node CREATED="1366547010194" ID="ID_760856852" MODIFIED="1366550285531" POSITION="right" TEXT="System- und Softwareentwurf">
+<node CREATED="1366547309509" ID="ID_60991598" MODIFIED="1366547311715" TEXT="Architektur">
+<node CREATED="1366547505910" ID="ID_25614333" MODIFIED="1366547509308" TEXT="Begr&#xfc;ndung Architektur"/>
+</node>
+</node>
+<node CREATED="1366547316237" ID="ID_1820667401" MODIFIED="1366550285002" POSITION="right" TEXT="Implementierung">
+<node CREATED="1366547486370" ID="ID_1675955707" MODIFIED="1366547491489" TEXT="Welche Besonderheiten"/>
+<node CREATED="1366547491893" ID="ID_47085448" MODIFIED="1366547493030" TEXT="Probleme"/>
+<node CREATED="1366547493660" ID="ID_1673327973" MODIFIED="1366547497473" TEXT="Integration"/>
+</node>
+</node>
+</map>
